edf1adae1b1174542374db0fb75171fd21cc5483
OnoderaTakuma <>
2026-03-16 01:55:03 +0900

    注目作品の部分を実装

--- src/features/movies/api/getTrendingMovies.ts
+++ src/features/movies/api/getTrendingMovies.ts
@@ -1,10 +1,5 @@
 import { MovieListResponse } from "@/features/movies/types/trending";
 import { fetchTMDB } from "@/lib/tmdb";
 
-const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-
-export async function getTrendingMovies(): Promise<MovieListResponse> {
-  return fetchTMDB<MovieListResponse>(
-    "/trending/movie/week"
-  )
-}
+export const getTrendingMovies = async (): Promise<MovieListResponse> =>
+  fetchTMDB<MovieListResponse>("/trending/movie/week");
--- src/features/movies/hooks/useTrendingMovies.ts
+++ src/features/movies/hooks/useTrendingMovies.ts
@@ -1,3 +1,5 @@
+"use client"
+
 import { useQuery } from "@tanstack/react-query";
 import { getTrendingMovies } from "@/features/movies/api/getTrendingMovies"
 import { MovieListResponse } from "@/features/movies/types/trending"
