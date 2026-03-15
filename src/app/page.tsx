3b8e73d1db7b545b17e55928ffbf5a1acf3b57a7
OnoderaTakuma <>
2026-03-16 01:56:02 +0900

    コンポーネントを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,41 @@
-import { HeroMovie } from "@/features/movies/components/HeroMovie";
+"use client"
 
-export default function Home() {
+import HeroMovie from "@/features/movies/components/HeroMovie";
+import MovieRow from "@/features/movies/components/MovieRow";
+
+import { useTrendingMovies } from "@/features/movies/hooks/useTrendingMovies"
+import { usePopularMovies } from "@/features/movies/hooks/usePopularMovies"
+import { useNowPlayingMovies } from "@/features/movies/hooks/useNowPlayingMovies";
+
+export default function Page() {
+  const { data: trending } = useTrendingMovies()
+  const { data: popular } = usePopularMovies()
+  const { data: nowPlaying } = useNowPlayingMovies()
+
   return (
     <>
       <HeroMovie />
+
+      {trending && (
+        <MovieRow
+          title="注目作品"
+          movies={trending.results}
+        />
+      )}
+
+      {popular && (
+        <MovieRow
+          title="人気作品"
+          movies={popular.results}
+        />
+      )}
+
+      {nowPlaying && (
+        <MovieRow
+          title="上映中作品"
+          movies={nowPlaying.results}
+        />
+      )}
     </>
-  );
-};
+  )
+}
